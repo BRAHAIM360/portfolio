5085e96813d4ddfd47064d4e847ef973c67b4658
Brahim Mahioussi <brahim.real.360@hotmail.fr>
2022-07-13 11:42:22 +0100

    commit

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -73,22 +73,22 @@ const About = () => {
 
         <div className="stage-cube-cont">
           <div className="cubespinner">
-            <div className="face1">
+            <div id="face1">
               <FontAwesomeIcon icon={faNode} color="#3c873a" />
             </div>
-            <div className="face2">
+            <div id="face2">
               <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
-            <div className="face3">
+            <div id="face3">
               <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
             </div>
-            <div className="face4">
+            <div id="face4">
               <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
             </div>
-            <div className="face5">
+            <div id="face5">
               <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
             </div>
-            <div className="face6">
+            <div id="face6">
               <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
             </div>
           </div>
